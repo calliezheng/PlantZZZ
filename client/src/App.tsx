7bc6ca5e4a0c45e1cda8a082bddc7f3410e91042
calliezheng <139871591+calliezheng@users.noreply.github.com>
2024-03-16 02:17:52 +1300

    set database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
 import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/home").then((response) => {
+    console.log(response);
+    });
+  }, []);
+ 
   return (
     <div className="App">
       
