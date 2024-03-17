5a2bc6d50c245384725d23bfb2dca1ca613844c5
calliezheng <139871591+calliezheng@users.noreply.github.com>
2024-03-18 00:46:35 +1300

    add link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/Home";
+import Sign from "./pages/Sign";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link to=""> Sign up/ Sign in</Link>
+        <Link to="/sign"> Sign up/ Sign in </Link>
+        <Link to="/"> Home </Link>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/sign" element={<Sign />} />
         </Routes>
       </Router>
     </div>
---
+++ client/src/pages/Sign.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sign() {
+  return (
+    <div>
+      Sign up/ Sign in
+    </div>
+  )
+}
+
+export default Sign
