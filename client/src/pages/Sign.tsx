178b5fa6e80783ab5de1bc833d7bb2c4bf8140db
calliezheng <139871591+calliezheng@users.noreply.github.com>
2024-03-19 00:22:18 +1300

    add form

--- client/src/pages/Sign.tsx
+++ client/src/pages/Sign.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
 
 function Sign() {
+  
+  interface FormValues {
+    username: string;
+    password: string;
+  }
+  
+  const initialValues:FormValues = {
+    username:'',
+    password:'',
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required(),
+    password: Yup.string().min(6).max(20).required(),
+  });
+  
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
   return (
-    <div>
-      Sign up/ Sign in
+    <div className='signUpForm'>
+      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+        <Form>
+          <label>Username</label>
+          <Field id="username" name="username" placeholder="Username or Email"/>
+          <label>Password</label>
+          <Field id="password" name="password" placeholder="Password"/>
+          <button>Submit</button>
+        </Form>
+      </Formik>
     </div>
   )
 }
 
-export default Sign
+export default Sign;
