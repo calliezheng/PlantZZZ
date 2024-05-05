d67b117054948689926989e70e0f94ff0720bbd9
calliezheng <139871591+calliezheng@users.noreply.github.com>
2024-05-05 14:25:19 +1200

    delete test picture

--- server/routes/plant.js
+++ server/routes/plant.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require('multer');
+const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 const { sequelize, Plant, Picture } = require("../models");
