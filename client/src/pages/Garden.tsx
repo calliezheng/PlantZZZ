d4f2f63545af50800e0f6905538c113c4f26a53d
calliezheng <139871591+calliezheng@users.noreply.github.com>
2024-05-16 00:16:34 +1200

    Update Garden.tsx

--- client/src/pages/Garden.tsx
+++ client/src/pages/Garden.tsx
@@ -37,6 +37,20 @@ function Garden() {
   const [products, setProducts] = useState<Product[]>([]);;
   const id = localStorage.getItem('user_id');
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+        try {
+            const response = await axios.get(`http://localhost:3001/store/${id}/cart`);
+            console.log(response);
+            setProducts(response.data);
+        } catch (error) {
+            console.error('Failed to fetch purchases:', error);
+        }
+    };
+
+    fetchProducts();
+}, [id]);
+
   const moveProductToGarden = (product: Product, destination: DraggableLocation) => {
   const newGrid = Array.from(grid); // Shallow copy of the grid
   const destinationCoords = destination.droppableId.match(/cell-(\d+)-(\d+)/);
@@ -89,20 +103,6 @@ const moveProductToItems = (source: DraggableLocation, product: Product) => {
     }
   }
   console.log(`Product ${product.product_id} moved back to items. New state:`, products);
-};
-
-
-const updateProductQuantity = async (productId:number, increment: boolean) => {
-  const payload = {
-    productId: productId,
-    increment: increment
-  };
-  try {
-    await axios.post(`http://localhost:3001/garden/${id}`, payload);
-    console.log("Quantity updated successfully");
-  } catch (error) {
-    console.error("Failed to update quantity:", error);
-  }
 };
 
 const moveItemWithinGarden = (source: DraggableLocation, destination: DraggableLocation) => {
@@ -184,20 +184,18 @@ const onDragEnd = (result: DropResult) => {
   }
 };
 
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-        try {
-            const response = await axios.get(`http://localhost:3001/store/${id}/cart`);
-            console.log(response);
-            setProducts(response.data);
-        } catch (error) {
-            console.error('Failed to fetch purchases:', error);
-        }
-    };
-
-    fetchProducts();
-}, [id]);
+const updateProductQuantity = async (productId:number, increment: boolean) => {
+  const payload = {
+    productId: productId,
+    increment: increment
+  };
+  try {
+    await axios.post(`http://localhost:3001/garden/${id}`, payload);
+    console.log("Quantity updated successfully");
+  } catch (error) {
+    console.error("Failed to update quantity:", error);
+  }
+};
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
