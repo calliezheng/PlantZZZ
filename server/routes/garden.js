98c9a78b467d91ba7fae7284e0644eb06826dae9
calliezheng <139871591+calliezheng@users.noreply.github.com>
2024-05-16 00:22:56 +1200

    Update garden.js

--- server/routes/garden.js
+++ server/routes/garden.js
@@ -3,9 +3,29 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 const router = express.Router();
-const { User, Product, Product_Type, Purchase, sequelize } = require("../models");
+const { Purchase, sequelize } = require("../models");
 
-
+router.get("/:id", async (req, res) => {
+    try {
+        const userId = req.params.id;  
+        const item_quantity = await Purchase.findOne({
+            where: {
+              user_id: userId
+            }
+          });
+          
+        
+        if (user) {
+            res.json({ score: user.score });  // Return the score if the user is found
+            console.log(user.score);
+        } else {
+            res.status(404).send('User not found');  // Send a 404 response if no user is found
+        }
+    } catch (error) {
+        console.error('Error fetching score:', error);
+        res.status(500).send('Error fetching score');
+    }
+});
 
 
 
